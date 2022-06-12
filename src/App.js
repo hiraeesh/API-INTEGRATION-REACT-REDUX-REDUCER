254187e7c3aba804713b2daf2612bfe203804c5f
Raeesh Rayeen <raeeshrayeen98@gmail.com>
2022-06-12 21:40:48 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-   <>
-   <h1>first update</h1>
-   </>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      value:''
+    };
+    this.textinput=React.createRef()
+  }
+  componentDidMount(){
+    this.textinput.current.blur()
+  }
+
+  handle = e =>{
+    e.preventDefault();
+    this.setState({value:this.textinput.current.value});
+    console.log(this.textinput.current.value)
+  };
+  render() {
+    return (
+      <>
+      <h1>This is PASSWORD {this.state.value}</h1>
+     <form onSubmit={this.handle}>
+    
+     PASSWORD:
+     <input type='text' ref={this.textinput} />
+     <input type='submit' value='submit' />
+     </form>
+    </>
+    )
+
+  }
 }
-
-export default App;
